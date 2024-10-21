c6d0f6e5e0ef459085f4c7af0c527eab100888a7
Hugo Rangel <hr2780921@gmail.com>
2024-10-21 14:05:02 -0600

    INDEX JS

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const eventRoutes = require('./routes/eventRoutes')
+
+//Const reservaRoutes = requiere('./routes/reservaRoutes');
+
+const db = require('./config/db');
+
+const app = express();
+app.use(express.json());
+
+app.use('/api/eventos',eventRoutes);
+
+//app.use('/api/eventos',reservaRoutes);
+
+const PORT= process.env.PORT || 3000;
+
+app.listen(PORT,() => {
+    console.info(`Servidor corriendo en el puerto` + PORT)
+});
